ba8da376e821141fbf4704ec629aef128e9763f1
Lunes313 <99741938+Lunes313@users.noreply.github.com>
2024-09-08 20:20:32 -0500

    logica login

--- Login/app.py
+++ Login/app.py
@@ -4,12 +4,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/procesar_datos', methods=['GET'])
+@app.route('/procesar_datos', methods=['POST'])
 def procesar_datos():
-    #Obtener datos del formulario
+    # Obtener datos del formulario
+    data = request.get_json()
+    nombre = data.get('nombre')
 
-    return jsonify({"mensaje": "mensaje Hola mundo"})
-
+    return jsonify({"mensaje": nombre})
 
 
 if __name__ == '__main__':
